1c14346c77345de3880be621f31330fd9ef0ef9e
Cwl <1666564131@qq.com>
2022-04-04 00:07:42 +0800

    first commit

---
+++ src/main/java/com/hj/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.hj.git;
+
+/**
+ * @ClassName : GitTest
+ * @Description 实现功能 :
+ * @Author : cwl
+ * @Date : 2022/4/4  0:03
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
